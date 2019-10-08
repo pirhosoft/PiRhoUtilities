700437a8c5da668a62ea873396b70ddf4832ba04
Luke Larson <luke.larson@gmail.com>
2019-10-08 11:29:40 -0700

    updated samples

--- Assets/PiRhoUtilities/Samples~/PiRhoUtilitySamples/Scripts/ExecutionSample.cs
+++ Assets/PiRhoUtilities/Samples~/PiRhoUtilitySamples/Scripts/ExecutionSample.cs
@@ -7,9 +7,11 @@ namespace PiRhoSoft.Utilities
 	{
 		[Button(nameof(Clicked), Label = "Click")]
 		[ChangeTrigger(nameof(Changed))]
+		[InspectTrigger(nameof(Inspect))]
 		public bool Toggle;
 
 		private void Clicked() => Debug.Log("Clicked", this);
 		private void Changed(bool oldValue, bool newValue) => Debug.Log($"Changed from {oldValue} to {newValue}", this);
+		private void Inspect() => Debug.Log("Object selected", this);
 	}
 }
