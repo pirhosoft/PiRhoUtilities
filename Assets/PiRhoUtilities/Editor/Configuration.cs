30fffda5e5e0edb7e6b5c1c897143a98a4f2a004
Luke Larson <luke.larson@gmail.com>
2019-10-08 12:37:42 -0700

    fixed configuration path

--- Assets/PiRhoUtilities/Editor/Configuration.cs
+++ Assets/PiRhoUtilities/Editor/Configuration.cs
@@ -2,23 +2,6 @@
 {
 	public static class Configuration
 	{
-		private static string _elementsPath = null;
-		private const string _elementsFolder = "Elements/";
-		private const string _editorFolder = "PiRhoUtilities/Scripts/Editor/";
-
-		internal static string ElementsPath
-		{
-			get
-			{
-				if (_elementsPath == null)
-					_elementsPath = AssetHelper.FindEditorPath(nameof(Configuration), _editorFolder) + _elementsFolder;
-
-				return _elementsPath;
-			}
-			set
-			{
-				_elementsPath = value; // settable so PiRho Utilities can be moved or renamed by end users
-			}
-		}
+		internal const string ElementsPath = "Packages/PiRhoUtilities/Editor/Elements/";
 	}
 }
