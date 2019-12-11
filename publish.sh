8068fd53141764305fedd877ce7a52e3d0d75703
Luke Larson <luke.larson@gmail.com>
2019-12-11 11:46:47 -0800

    added reverting samples commit to script to return to a clean working directory

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 mv Assets/PiRhoUtilities/Samples Assets/PiRhoUtilities/Samples~
-mv Assets/PiRhoUtilities/Samples.meta Assets/Samples.mata
+mv Assets/PiRhoUtilities/Samples.meta Assets/Samples.meta
 cp -f README.md Assets/PiRhoUtilities
 
 git add .
@@ -7,4 +7,8 @@ git commit -m "publishing branch"
 git subtree push --prefix Assets/PiRhoUtilities origin upm
 
 mv Assets/PiRhoUtilities/Samples~ Assets/PiRhoUtilities/Samples
-mv Assets/Samples.meta Assets/PiRhoUtilities/Samples.mata
+mv Assets/Samples.meta Assets/PiRhoUtilities/Samples.meta
+
+git add .
+git commit -m "reverting samples"
+git push
