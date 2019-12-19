b050f4ff496395a0c34ca21fcb8a9f3bb1c4926b
Adam Larson <adam@pirhosoft.com>
2019-12-19 14:30:54 -0800

    IconButton uxml support with code and uxml sample

--- Editor/Elements/IconButton/IconButton.cs
+++ Editor/Elements/IconButton/IconButton.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,6 +7,12 @@ namespace PiRhoSoft.Utilities.Editor
 {
 	public class IconButton : Image
 	{
+		#region Log Messages
+
+		private const string _missingIconWarning = "(PUEIBMI) unknown icon '{0}' for IconButton: the icon could not be found";
+
+		#endregion 
+
 		#region Class Names
 
 		public const string Stylesheet = "IconButton/IconButton.uss";
@@ -50,8 +57,52 @@ namespace PiRhoSoft.Utilities.Editor
 
 		#region UXML Support
 
+		private static readonly Dictionary<string, Icon> _icons = new Dictionary<string, Icon>
+		{
+			{ "Add", Icon.Add },
+			{ "CustomAdd", Icon.CustomAdd },
+			{ "Remove", Icon.Remove },
+			{ "Inspect", Icon.Inspect },
+			{ "Expanded", Icon.Expanded },
+			{ "Collapsed", Icon.Collapsed },
+			{ "Refresh", Icon.Refresh },
+			{ "Load", Icon.Load },
+			{ "Unload", Icon.Unload },
+			{ "Close", Icon.Close },
+			{ "LeftArrow", Icon.LeftArrow },
+			{ "RightArrow", Icon.RightArrow },
+			{ "Info", Icon.Info },
+			{ "Warning", Icon.Warning },
+			{ "Error", Icon.Error },
+			{ "Settings", Icon.Settings },
+			{ "View", Icon.View },
+			{ "Locked", Icon.Locked },
+			{ "Unlocked", Icon.Unlocked }
+		};
+
 		public new class UxmlFactory : UxmlFactory<IconButton, UxmlTraits> { }
-		public new class UxmlTraits : Image.UxmlTraits { }
+		public new class UxmlTraits : Image.UxmlTraits
+		{
+			private readonly UxmlStringAttributeDescription _icon = new UxmlStringAttributeDescription { name = "icon", defaultValue = "Add" };
+
+			public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+			{
+				base.Init(ve, bag, cc);
+
+				var button = ve as IconButton;
+				var name = _icon.GetValueFromBag(bag, cc);
+
+				if (_icons.TryGetValue(name, out var icon))
+				{
+					button.image = icon.Texture;
+				}
+				else
+				{
+					Debug.LogWarningFormat(_missingIconWarning, name);
+					button.image = Icon.Add.Texture;
+				}
+			}
+		}
 
 		#endregion
 	}
