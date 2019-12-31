d22012cb5397d460307dc162dc385fe61d70c4c2
Luke Larson <luke.larson@gmail.com>
2019-12-30 17:04:55 -0800

    reference sample, frame sample, inline sample

--- Samples/PiRhoUtilitySamples/Scripts/Runtime/Containers/InlineSample.cs
+++ Samples/PiRhoUtilitySamples/Scripts/Runtime/Containers/InlineSample.cs
@@ -1,9 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace PiRhoSoft.Utilities
 {
 	[AddComponentMenu("PiRho Utilities/Inline")]
 	public class InlineSample : MonoBehaviour
 	{
+		[Serializable]
+		public class MembersClass
+		{
+			public bool Member1;
+			public int Member2;
+			public float Member3;
+			public string Member4;
+		}
+
+		[Serializable]
+		public class WrapperClass
+		{
+			public int Value;
+		}
+
+		[MessageBox("The [Inline] attribute displays a serializable class directly inline. Member labels can be optionally shown.", MessageBoxType.Info, Location = TraitLocation.Above)]
+
+		[Inline]
+		public MembersClass Members;
+
+		[Inline(false)]
+		public WrapperClass Wrapper;
 	}
 }
--- Samples/PiRhoUtilitySamples/Scripts/Runtime/Containers/ReferenceSample.cs
+++ Samples/PiRhoUtilitySamples/Scripts/Runtime/Containers/ReferenceSample.cs
@@ -1,9 +1,41 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace PiRhoSoft.Utilities
 {
 	[AddComponentMenu("PiRho Utilities/Reference")]
 	public class ReferenceSample : MonoBehaviour
 	{
+		public interface IAnimal
+		{
+		}
+
+		[Serializable]
+		public class Dog : IAnimal
+		{
+			public int Teeth;
+		}
+
+		[Serializable]
+		public class Cat : IAnimal
+		{
+			public int Claws;
+		}
+
+		[Serializable]
+		public class Hippo : IAnimal
+		{
+			public int Blubber;
+		}
+
+		[MessageBox("The [Reference] attribute is applied in addition to Unity's [SerializeReference] attribute to enable creating and editing of any valid subtype for the field.", MessageBoxType.Info, Location = TraitLocation.Above)]
+
+		[SerializeReference]
+		[Reference]
+		public IAnimal AnimalReference;
+
+		[SerializeReference]
+		[Reference]
+		public IAnimal AnimalReference2;
 	}
 }
