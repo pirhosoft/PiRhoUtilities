cba1febe44ae7cbbfab2caa7f6f65cff3dafc24b
Luke Larson <luke.larson@gmail.com>
2019-12-30 16:28:31 -0800

    fixed icon button bug

--- Assets/PiRhoUtilities/Samples/PiRhoUtilitySamples/Scripts/Runtime/Containers/GroupSample.cs
+++ Assets/PiRhoUtilities/Samples/PiRhoUtilitySamples/Scripts/Runtime/Containers/GroupSample.cs
@@ -5,5 +5,13 @@ namespace PiRhoSoft.Utilities
 	[AddComponentMenu("PiRho Utilities/Group")]
 	public class GroupSample : MonoBehaviour
 	{
+		[MessageBox("The [Group] attribute displays fields together in a frame like view.", MessageBoxType.Info, Location = TraitLocation.Above)]
+
+		[Group("One")] public int Int1;
+		[Group("One")] public float Float1;
+		[Group("Two")] [Maximum(100)] public float Float2;
+		[Group("One")] public bool Bool1;
+		[Group("Two")] public bool Bool2;
+		[Group("Two")] public int Int2;
 	}
 }
--- Assets/PiRhoUtilities/Samples/PiRhoUtilitySamples/Scripts/Runtime/Containers/TabsSample.cs
+++ Assets/PiRhoUtilities/Samples/PiRhoUtilitySamples/Scripts/Runtime/Containers/TabsSample.cs
@@ -5,5 +5,13 @@ namespace PiRhoSoft.Utilities
 	[AddComponentMenu("PiRho Utilities/Tabs")]
 	public class TabsSample : MonoBehaviour
 	{
+		[MessageBox("The [Tabs] attribute displays fields together in a tab like view.", MessageBoxType.Info, Location = TraitLocation.Above)]
+
+		[Tabs("Tabs", "One")] public int Int1;
+		[Tabs("Tabs", "One")] public float Float1;
+		[Tabs("Tabs", "Two")] [Maximum(100)] public float Float2;
+		[Tabs("Tabs", "One")] public bool Bool1;
+		[Tabs("Tabs", "Two")] public bool Bool2;
+		[Tabs("Tabs", "Two")] public int Int2;
 	}
 }
