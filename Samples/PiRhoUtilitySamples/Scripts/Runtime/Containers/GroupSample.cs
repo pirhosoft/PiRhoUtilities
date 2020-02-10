d51ea0f72b21254ae0b03a3dfd22f096b41f5534
Luke Larson <luke.larson@gmail.com>
2020-02-10 15:20:55 -0800

    added images to documentation

--- Samples/PiRhoUtilitySamples/Scripts/Runtime/Containers/GroupSample.cs
+++ Samples/PiRhoUtilitySamples/Scripts/Runtime/Containers/GroupSample.cs
@@ -7,11 +7,11 @@ namespace PiRhoSoft.Utilities
 	{
 		[MessageBox("The [Group] attribute displays fields together in a frame like view.", MessageBoxType.Info, Location = TraitLocation.Above)]
 
-		[Group("One")] public int Int1;
-		[Group("One")] public float Float1;
-		[Group("Two")] [Maximum(100)] public float Float2;
-		[Group("One")] public bool Bool1;
-		[Group("Two")] public bool Bool2;
-		[Group("Two")] public int Int2;
+		[Group("Group One")] public int Int1;
+		[Group("Group One")] public float Float1;
+		[Group("Group Two")] [Maximum(100)] public float Float2;
+		[Group("Group One")] public bool Bool1;
+		[Group("Group Two")] public bool Bool2;
+		[Group("Group Two")] public int Int2;
 	}
 }
